fbb2939389c6b23e8f6fc99a3a5cf1396a73e4ef
Gustavo <gteixeirabreda@gmail.com>
2022-12-29 09:51:55 -0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/*botao de passar foto*/
+/*ver todos funcionando*/
+/*som ao enviar formulario*/
+/*som de clique ao clicar nas diferentes abas e fazer isso com listas e referencias*/
+/*usar função anonima*/
+/* id pra cada um e fzr ids dinamico com crase*/
+/* colocar tecla enter*/
+/* refazer todas as classes*/
